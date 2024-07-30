c47b7a722df194f0e77fabda131d9f591c4ca47e
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-07-30 19:59:07 +0530

    tf

---
+++ Terraform/ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "this_inst" {
+  ami                     = "ami-0427090fd1714168b"
+  instance_type           = "t2.micro"
+  availability_zone       = "us-east-1a"
+  disable_api_termination = "true"
+  key_name                = "anupnvdev"
+  security_groups         = "default"
+  count = "2"
+  #vpc_security_group_ids  = "sg-09d5d9601120f72dd"
+  volume_size   = 10GiB
+  volume_type   = "gp3"
+
+
+
+
+    tags = {
+      Name = "TerraEC2"  
+      #batch =  
+      #strength = 
+  }
+ 
+}
