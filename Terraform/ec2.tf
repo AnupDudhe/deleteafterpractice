7e7a3c26eeb02e821ee3b6bc4af06a221ed384cc
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-01 20:23:21 +0530

    co

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "this_inst" {
   disable_api_termination = var.This_api_termination
   key_name                = var.This_map.keyone
   #security_groups         = "default"
-  count = var.This_list.1
+  #count = var.This_list.1
   vpc_security_group_ids  = ["sg-09d5d9601120f72dd"]
   #volume_size   = "10GiB"
   #volume_type   = "gp3"
