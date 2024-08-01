de5e1ea6eadda7dc81f1139e4cb4bd9f58428b64
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-01 20:40:08 +0530

    commit

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this_inst" {
-  ami                     = var.This_list.0
+  ami                     = 
   instance_type           = var.This_any.string
   availability_zone       = var.This_map.keytwo
   disable_api_termination = var.This_api_termination
