6a0044c64199dfd01112a51fc5d02c36ddfbf470
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-02 20:09:52 +0530

    profile

--- Terraform/Module/Maindev/profile.tf
+++ Terraform/Module/Maindev/profile.tf
@@ -1,1 +1,7 @@
 
+#profie is nothing but the user credentials that will be used to create infra in the mentioned cloud provider 
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "configs"
+}
