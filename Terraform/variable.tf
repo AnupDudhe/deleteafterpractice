1254692520d2a3c84467347dc63133cae2d5a449
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-02 20:40:22 +0530

    tesy

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,13 +1,4 @@
-variable "This_Aws_UserName" {
-    description = "user name"
-    type = string
-    default = "sai"
-}
-variable "This_Path" {
-    description = "path"
-    type = string
-    default = "/"
-}
+
 
 variable "This_api_termination" {
     description = "for api termination"
