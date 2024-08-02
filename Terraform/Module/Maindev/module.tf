deb764bc403e47e6e6b46e65d000d8f5a56323f1
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-02 20:49:18 +0530

    az

--- Terraform/Module/Maindev/module.tf
+++ Terraform/Module/Maindev/module.tf
@@ -2,8 +2,8 @@ module "ec2_module" {
         source = "/home/cloudshell-user/deleteafterpractice/Terraform/Module/resources/EC2"
         This_api_termination = true
         This_map = "anupnvdev"
-        This_az  = "us-east-1a"
+        #This_az  = "us-east-1a"
         This_list =  "webapp"
         This_any = "t2.micro"
-        This_sg  = "default"
+        #This_sg  = "default"
 }
