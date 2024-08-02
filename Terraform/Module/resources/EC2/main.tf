6c6091d4399e6fdfa6b0e17f4c53d3b0bc8a853c
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-02 20:11:52 +0530

    commit

--- Terraform/Module/resources/EC2/main.tf
+++ Terraform/Module/resources/EC2/main.tf
@@ -1,1 +1,17 @@
 
+resource "aws_instance" "this_inst" {
+  ami                     = 
+  instance_type           = var.This_any.string
+  availability_zone       = var.This_map.keytwo
+  disable_api_termination = var.This_api_termination
+  key_name                = var.This_map.keyone
+  security_groups         = "default"
+  #count = var.This_list.1
+  vpc_security_group_ids  = ["sg-09d5d9601120f72dd"]
+  #volume_size   = "10GiB"
+  #volume_type   = "gp3"
+    tags = {
+      Name = "TerraEC2"   
+  }
+ 
+}
