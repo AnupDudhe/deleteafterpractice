f17d3b2022d8e3eec359510f35e59485dd7cd7d1
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-02 20:19:30 +0530

    comment

--- Terraform/Module/resources/EC2/main.tf
+++ Terraform/Module/resources/EC2/main.tf
@@ -6,10 +6,15 @@ resource "aws_instance" "this_inst" {
   disable_api_termination = var.This_api_termination
   key_name                = var.This_map.keyone
   security_groups         = "default"
-  count = var.This_list.1
+  #count = var.This_list.1
   vpc_security_group_ids  = [var.This_list.2]
     tags = {
       Name = var.This_list.3
   }
  
+}
+
+
+output "ec2_outputs" {
+  value = aws_instance.this_inst.public_ip
 }
