fa40281d1ac9d5e9aebc252cda63de77d24397d0
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-03 20:25:16 +0530

    Create variable.tf

---
+++ Terraform/Module/resources/VPC/variable.tf
@@ -0,0 +1,22 @@
+
+variable "This_api_termination" {
+}
+
+variable "This_key" {   
+}
+
+variable "This_az" {    
+
+}
+
+variable "This_tag" {   
+}
+
+variable "This_inst" {    
+}
+
+variable "This_sg" {   
+}
+
+variable "This_ami" {    
+}
