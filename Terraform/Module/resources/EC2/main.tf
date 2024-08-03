126de406a6b077947ba8f5ce6c2db3d76d11b1ca
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-03 20:46:18 +0530

    tes

--- Terraform/Module/resources/EC2/main.tf
+++ Terraform/Module/resources/EC2/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "this_inst" {
   availability_zone       = var.This_az
   disable_api_termination = var.This_api_termination
   key_name                = var.This_key
-  vpc_security_group_ids  = var.This_sg
+  #vpc_security_group_ids  = var.This_sg
   tags = {
       Name = var.This_tag
      }
