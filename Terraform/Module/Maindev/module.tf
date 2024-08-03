248c2fb1ed45107a4536bb8f915faf4c383acd35
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-03 20:09:29 +0530

    test

--- Terraform/Module/Maindev/module.tf
+++ Terraform/Module/Maindev/module.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-        source = "/home/cloudshell-user/deleteafterpractice/Terraform/Module/resources/EC2"
+        source = "/home/anup/deleteafterpractice/Terraform/Module/resources/EC2"
         This_api_termination = true
         This_key = "anupnvdev"
         This_az  = "us-east-1a"
@@ -8,5 +8,5 @@ module "ec2" {
         This_sg  = ["sg-09d5d9601120f72dd"]
         This_ami = "ami-0ba6a031e08a05649"
 
-        
+
 }
