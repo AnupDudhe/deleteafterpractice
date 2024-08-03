7e374eaefa800e4a257108e68520fd3b1016decd
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-03 20:02:25 +0530

    commit

--- Terraform/Module/resources/EC2/variable.tf
+++ Terraform/Module/resources/EC2/variable.tf
@@ -1,47 +1,22 @@
-variable "This_Aws_UserName" {
-    description = "user name"
-    type = string
-    default = "sai"
-}
-variable "This_Path" {
-    description = "path"
-    type = string
-    default = "/"
-}
 
 variable "This_api_termination" {
-    description = "for api termination"
-    type = bool
-    default = true
 }
 
-variable "This_count" {
-    description = "for ec2 count"
-    type = number
-    default = 1
+variable "This_key" {   
 }
 
-variable "This_map" {
-    description = "for ec2 count"
-    type = map
-    default = {
-        keyone = "anupnvdev" ,
-        keytwo = "us-east-1a"
-    }
+variable "This_az" {    
+
 }
 
-variable "This_list" {
-    description = "for ec2 count"
-    type = list
-    default = ["ami-0427090fd1714168b",3]
+variable "This_tag" {   
 }
 
-variable "This_any" {
-    description = "for ec2 count"
-    type = any
-    default = {
-       string = "t2.micro"
-       
-    }
+variable "This_inst" {    
 }
 
+variable "This_sg" {   
+}
+
+variable "This_ami" {    
+}
