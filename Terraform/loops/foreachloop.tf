696c047e6bf3d5f24030b686930a651e62a724be
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-06 20:18:56 +0530

    test

--- Terraform/loops/foreachloop.tf
+++ Terraform/loops/foreachloop.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "this_aws_instance" {
     for_each = toset(var.aws_ami)
     ami = each.value
-    key_name = "mumbai-region"
     instance_type = "t2.micro"
 
     
