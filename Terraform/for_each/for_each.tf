59cdb172957770bec5488288cff2a5308bdff7d6
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-06 20:26:37 +0530

    test3

---
+++ Terraform/for_each/for_each.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_user" "main_user1" {
+    name =  var.aws_user_name[count.index]
+    count = length(var.aws_user_name)
+}
+
+ variable "aws_user_name" {
+    type = list(string)
+    default =  ["ubuntu","centos","mintos","solaris"]
+} 
