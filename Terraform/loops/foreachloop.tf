f52ef15fbfac9fe0a5694a310619902bda12f171
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-08-06 20:34:34 +0530

    test

--- Terraform/loops/foreachloop.tf
+++ Terraform/loops/foreachloop.tf
@@ -14,3 +14,9 @@ variable aws_ami {
        type = list(string)
        default = ["ami-03972092c42e8c0ca", "ami-0583d8c7a9c35822c", "ami-0b247d4d0226ca7cd" , "ami-04a81a99f5ec58529" ]
 }
+
+
+output "aws_public_ip" {
+    value = [for instance in var.aws_ami:
+    aws_instance.this_aws_instance[instance].public_ip]
+}
