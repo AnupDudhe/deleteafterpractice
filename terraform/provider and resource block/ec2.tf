a207a043aad1d3ee906a20b3a7d822d9cf8a696a
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-10-28 09:40:50 +0530

    ec2

--- terraform/provider and resource block/ec2.tf
+++ terraform/provider and resource block/ec2.tf
@@ -1,8 +1,15 @@
+resource "aws_instance" "this_ubuntu" {
 
-
-
-resource "aws_instance" "this" {
-
-
+    ami = ami-0325498274077fac5 
+    disable_api_stop  = true 
+    disable_api_termination = true  
+    instance_type = t2.micro 
+    key_name = mumbaianupb38kp
+    security_groups = [sg-0206fe395dc0f6d38]
+    count = 4  #loop 
+    tags = {
+      purpose = "jenkins server"
+    } 
+    
 
 }    
