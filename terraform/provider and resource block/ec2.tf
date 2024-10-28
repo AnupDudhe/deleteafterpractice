7138a63ad04a739a2bbdce36de36fda4f37a0e1f
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-10-28 09:51:12 +0530

    tr

--- terraform/provider and resource block/ec2.tf
+++ terraform/provider and resource block/ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "this_ubuntu" {
 
-    ami = ami-0325498274077fac5 
+    ami = "ami-0325498274077fac5" 
     disable_api_stop  = true 
     disable_api_termination = true  
-    instance_type = t2.micro 
-    key_name = mumbaianupb38kp
+    instance_type = "t2.micro" 
+    key_name = "mumbaianupb38kp"
     security_groups = [sg-0206fe395dc0f6d38]
     count = 4  #loop 
     tags = {
