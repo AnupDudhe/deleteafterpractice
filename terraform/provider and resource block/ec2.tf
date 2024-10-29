7be4bf223678621a74dd95c8611d5aff0bced1b4
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-10-29 10:18:44 +0530

    Update ec2.tf

--- terraform/provider and resource block/ec2.tf
+++ terraform/provider and resource block/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "this_ubuntu" {
 
     ami = "ami-050cd642fd83388e4" 
-    disable_api_stop  = true 
-    disable_api_termination = true  
+    disable_api_stop  = false
+    disable_api_termination = false  
     instance_type = "t2.micro" 
     #key_name=""
     vpc_security_group_ids = ["sg-09d0f3b967811ad23" , ]
