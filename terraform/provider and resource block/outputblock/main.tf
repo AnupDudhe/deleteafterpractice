9d2dd2a6e4af0e06b1e357240eb319fc16817a8d
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-12 09:41:53 +0530

    c

--- terraform/provider and resource block/outputblock/main.tf
+++ terraform/provider and resource block/outputblock/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "this_ubuntu" {
     instance_type = var.this_list[0]  #var.this_any.instance_type_list[0]
     #key_name=""
     vpc_security_group_ids = [var.this_vpc_security_group_ids]
-    count =    var.this_any.count   #var.this_count  #loop #var.this_list[1]
+    #count =    var.this_any.count   #var.this_count  #loop #var.this_list[1]
     tags = {
        purpose = var.this_map.purposeec2    #var.this_any.tags_map.purposeec2
        #purpose = "webserver"
