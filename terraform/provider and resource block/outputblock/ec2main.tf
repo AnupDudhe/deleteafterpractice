a17d9ee3523535600a591a6cdbfed33ae4110564
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-13 09:53:24 +0530

    test

--- terraform/provider and resource block/outputblock/ec2main.tf
+++ terraform/provider and resource block/outputblock/ec2main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "this_ubuntu" {
     disable_api_termination =  var.this_any.api_termination_ec2  #var.this_disable_api_termination  
     instance_type = var.this_list[0]  #var.this_any.instance_type_list[0]
     #key_name=""
-    vpc_security_group_ids = [var.this_vpc_security_group_ids]
+    #vpc_security_group_ids = [var.this_vpc_security_group_ids]
       depends_on = [aws_subnet.this_subnet_pub]
     subnet_id =  aws_subnet.this_subnet_pub.id
     #count =    var.this_any.count   #var.this_count  #loop #var.this_list[1]
