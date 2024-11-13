6fcc09615c5e2b529bf28a209d2130b8c7a1c06f
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-13 09:45:19 +0530

    test

--- terraform/provider and resource block/outputblock/ec2main.tf
+++ terraform/provider and resource block/outputblock/ec2main.tf
@@ -15,6 +15,26 @@ resource "aws_instance" "this_ubuntu" {
 
 }    
 
+
+resource "aws_vpc" "this_vpc" {
+    cidr_block = var.this_vpc_cidr_block //"12.11.0.0/16"
+    tags =  {
+        Name = var.this_vpc_tags //"this_vpc"
+    }
+}
+
+resource "aws_subnet" "this_subnet_pub" {
+  vpc_id     = aws_vpc.this_vpc.id  #referedresourceblock'sProvider_referedresourceblock'sresourcetype.referedresourceblocksuniqueresourceblockname.attributeofresource
+  availability_zone = "ap-south-1a"
+  cidr_block = var.this_subnet_pub_cidr_block      //"12.11.0.0/17"  
+  map_public_ip_on_launch = var.this_subnet_pub_map_ip //true
+  tags = {
+    Name = var.this_subnet_pub_tags //"pub_subnet"
+  }
+}
+
+
+
 output "vpcsubnet_id" {
     value = aws_subnet.this_subnet_pub.id 
 }
