23247dba3a8d4effcd61b90b2ccf17e8f78ff771
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-14 09:36:16 +0530

    tes

---
+++ terraform/provider and resource block/datablock/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "this_ubuntu" {
+    ami =  data.aws_ami.this_aws_ami   #var.this_image_id 
+    disable_api_stop  = var.this_any.api_stop_ec2  #var.this_disable_api_stop 
+    disable_api_termination =  var.this_any.api_termination_ec2  #var.this_disable_api_termination  
+    instance_type = var.this_list[0]  #var.this_any.instance_type_list[0]
+    #key_name=""
+    #vpc_security_group_ids = [var.this_vpc_security_group_ids]
+      depends_on = [aws_subnet.this_subnet_pub]
+    subnet_id =  aws_subnet.this_subnet_pub.id
+    #count =    var.this_any.count   #var.this_count  #loop #var.this_list[1]
+    tags = {
+       purpose = var.this_map.purposeec2    #var.this_any.tags_map.purposeec2
+       #purpose = "webserver"
+    } 
+    
+}  
---
+++ terraform/provider and resource block/datablock/variable.tf
@@ -0,0 +1,60 @@
+variable "this_image_id" {
+  type        = string
+  default = "ami-09b0a86a2c84101e1"
+}
+
+variable "this_disable_api_stop" {
+  type        = bool
+  default = false
+}
+variable "this_disable_api_termination" {
+    type = bool 
+    default = false
+    description = "this variable is used to pass bool data to api_termination" 
+}
+variable "this_count" {
+    type = number 
+    default = 2
+     
+}
+variable "this_vpc_security_group_ids" {
+    type = string 
+    default = "sg-0505874879e7ce6cd"
+     
+}
+
+variable "this_list" {
+    type = list 
+    default = ["t2.micro" , "2" , "false"]
+     
+}
+
+variable "this_map" {
+    type = map  
+    default = {
+     purposeec2 = "webserver"   
+     termination = true 
+     count = 1
+
+    }
+     
+}
+
+
+variable "this_any" {
+    type = any 
+    default = {
+        count = 1
+        api_termination_ec2 = false
+        api_stop_ec2 = false 
+        ami_ec2 = "ami-03753afda9b8ba740" 
+        instance_type_list = ["t2.micro" , "t3.small" , "t3.large"]
+        tags_map = {
+           purposeec2 = "webserver" 
+
+        }
+
+
+
+    }
+}
