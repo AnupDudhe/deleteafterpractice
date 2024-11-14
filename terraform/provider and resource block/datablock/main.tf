c6a4bbf3231123ace448b056d136187cc4920400
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-14 09:36:56 +0530

    id

--- terraform/provider and resource block/datablock/main.tf
+++ terraform/provider and resource block/datablock/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this_ubuntu" {
-    ami =  data.aws_ami.this_aws_ami   #var.this_image_id 
+    ami =  data.aws_ami.this_aws_ami.id   #var.this_image_id 
     disable_api_stop  = var.this_any.api_stop_ec2  #var.this_disable_api_stop 
     disable_api_termination =  var.this_any.api_termination_ec2  #var.this_disable_api_termination  
     instance_type = var.this_list[0]  #var.this_any.instance_type_list[0]
