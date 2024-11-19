e253f143187a05eb962cd93b230bb6bd6e304251
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-19 10:08:44 +0530

    test

--- Module/Resources/EC2/main.tf
+++ Module/Resources/EC2/main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "this_ubuntu" {
     disable_api_stop  = var.this_disable_api_stop  #var.this_disable_api_stop 
     disable_api_termination =  var.this_disable_api_termination  #var.this_disable_api_termination  
     instance_type = var.this_list  #var.this_any.instance_type_list[0]
-    #subnet_id =  var.this_aws_instance_subnet 
+    subnet_id =  var.this_aws_instance_subnet 
 }    
 
