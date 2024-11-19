e28af1658c4b15d7ac3fc509fc9043e5ed0b4c9d
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-19 09:32:01 +0530

    trd

--- Module/Dev/module.tf
+++ Module/Dev/module.tf
@@ -3,8 +3,9 @@ module "ec2" {
     source = "/home/cloudshell-user/deleteafterpractice/Module/Resources/EC2"
     this_image_id = "ami-09b0a86a2c84101e1"
     this_list = "t2.micro"
+    this_disable_api_stop = false
+    this_disable_api_termination = false
     #this_vpc_security_group_ids = "sg-0505874879e7ce6cd"
-   
     #this_aws_instance_subnet = module.vpc.subnet_id
     #this_sg_vpc_id = module.vpc.vpc_id
     //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
