247745f5c2084591bcf8a4c6f22312a99973a3ac
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-21 10:37:40 +0530

    Update main.tf

--- terraform/provider and resource block/backend/main.tf
+++ terraform/provider and resource block/backend/main.tf
@@ -19,7 +19,7 @@ terraform {
 
 
 resource "aws_instance" "ths_instance" {
-  ami = "ami-0a38c1c38a15fed74"
+  ami = "ami-012967cc5a8c9f891"
   //key_name = "anupdel"
   instance_type = "t2.micro"
   //security_groups = ["sg-02582bf615cdb71bb"]
