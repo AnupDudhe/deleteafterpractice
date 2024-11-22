b2197c10876a54fc5eec905e8209b08e0e3e0b98
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2024-11-22 09:40:42 +0530

    cl

---
+++ terraform/provider and resource block/loops/countloop.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+   profile = "configs"
+    default_tags {
+                tags = {
+                    name = "aws"
+                }
+    }
+}
+
+
+resource "aws_iam_user" "this_aws_iam_user" { 
+    count = length(var.users_list)  #3
+    name = var.users_list[count.index]
+}  
+resource "aws_iam_user" "this_aws_iam_user_2" { 
+    name = "neo.${count.index}"   neo
+    count = 3 
+}  
+
+
+variable "users_list" {
+    default = ["virat" , "rohit" , "raina"]
+}                #0         #1       #2
+  
