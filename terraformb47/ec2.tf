16b8f004c3548206aa4b282276121e676a202e62
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-12 19:50:29 +0530

    tec

--- terraformb47/ec2.tf
+++ terraformb47/ec2.tf
@@ -1,1 +1,10 @@
-
+resource "aws_instance" "ec2webserver" {
+  ami =  "ami-05c179eced2eb9b5b"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids = ["sg-0b1e8061bdcc3b242"]
+  key_name = "b25"
+  tags = {
+    name = "webserver"
+  }
+  subnet_id = "subnet-0ff38141a792616f5"
+}
