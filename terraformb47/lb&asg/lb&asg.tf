6c06e70eeda548d8dc5a979dd4dee1d58f45a4f8
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-18 19:55:44 +0530

    rw

--- terraformb47/lb&asg/lb&asg.tf
+++ terraformb47/lb&asg/lb&asg.tf
@@ -4,6 +4,6 @@ resource "aws_lb" "test" {
   load_balancer_type = var.this_lbtype[0]
   security_groups    = [var.this_sg]
   enable_deletion_protection = var.this_dp 	
-  subnets            = ["subnet-0ff38141a792616f5"] 
+  subnets            = ["subnet-0ff38141a792616f5" , "subnet-042374d63a6989a76"] 
 
 }
