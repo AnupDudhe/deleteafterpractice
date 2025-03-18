9fb8e4870b4255a97ed8864cd76d846ce2b21e87
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-18 20:17:27 +0530

    re

--- terraformb47/lb&asg/lb&asg.tf
+++ terraformb47/lb&asg/lb&asg.tf
@@ -13,3 +13,21 @@ output "lb_output" {
      description = "dns of lb"
      value       = aws_lb.test.dns_name
 }
+
+resource "aws_launch_template" "foobar" {
+  name_prefix   = "foobar"
+  image_id      = "ami-05c179eced2eb9b5b"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["ap-south-1a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.foobar.id
+    version = "$Latest"
+  }
+}
