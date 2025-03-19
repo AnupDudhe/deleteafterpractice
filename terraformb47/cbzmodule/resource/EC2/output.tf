32fcde520041072954cfd210a46e34497da92788
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-19 20:11:26 +0530

    sa

--- terraformb47/cbzmodule/resource/EC2/output.tf
+++ terraformb47/cbzmodule/resource/EC2/output.tf
@@ -1,28 +1,4 @@
+output "publicip" {
+   value = aws_instance.this_inst.public_ip
 
-
-variable "This_api_termination" {
-}
-
-variable "This_key" {   
-}
-
-variable "This_az" {    
-
-}
-
-variable "This_tag" {   
-}
-
-variable "This_inst" {    
-}
-
-
-
-variable "This_ami" {    
-}
-variable "this_aws_vpc_id" {
-    type = string
-}
-variable "this_aws_instance_subnet" {
-     type = string
 }
