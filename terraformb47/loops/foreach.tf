6c832d6fa7dad25185da461371e7030899ca3597
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-21 20:00:28 +0530

    dasd

--- terraformb47/loops/foreach.tf
+++ terraformb47/loops/foreach.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region =  "us-west-2"
+    region =  "ap-south-1"
     profile = "configs"
 }
  
@@ -7,8 +7,8 @@ provider "aws" {
 resource "aws_instance" "this_aws_instance" {
     for_each = toset(var.imageid)
     ami = each.value
-    vpc_security_group_ids = ["sg-032e1a4a1685a03be"]
-    key_name = "delete_oregon_anup"
+   // vpc_security_group_ids = ["sg-032e1a4a1685a03be"]
+   // key_name = "delete_oregon_anup"
     instance_type = "t2.micro"
     
 }   
