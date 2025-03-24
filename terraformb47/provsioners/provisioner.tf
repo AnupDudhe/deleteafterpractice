752b1fe970481e255ffdb6403351ad84e1fc43ba
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-24 20:16:50 +0530

    a

--- terraformb47/provsioners/provisioner.tf
+++ terraformb47/provsioners/provisioner.tf
@@ -1,37 +1,37 @@
- 
- provider "aws" {
-    region =  ""
-    //access_key = ""
-    //secret_key = ""
-    profile = "configs"
+
+provider "aws" {
+  region = "ap-south-1"
+  //access_key = ""
+  //secret_key = ""
+  profile = "configs"
 }
-  resource "aws_instance" "this_aws_instance" {
-    //for_each = toset(var.imageid)
-    ami = "ami-05c179eced2eb9b5b"
-    vpc_security_group_ids = ["sg-09ae24b53482e0b20"]
-    key_name = ""
-    instance_type = "t2.micro"
-     provisioner "file" {
+resource "aws_instance" "this_aws_instance" {
+  //for_each = toset(var.imageid)
+  ami                    = "ami-05c179eced2eb9b5b"
+  vpc_security_group_ids = ["sg-09ae24b53482e0b20"]
+  key_name               = "b25"
+  instance_type          = "t2.micro"
+  provisioner "file" {
     source      = "hello.txt"
     destination = "/home/ec2-user/aws/"
   }
-   provisioner "local-exec" {
+  provisioner "local-exec" {
     command = "echo ${self.private_ip} >> private_ips.txt"
   }
-    connection {
-    type     = "ssh"
-    user     = "ec2-user"
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
     private_key = file("${path.module}/id_rsa")
-    host     = "${self.public_ip}"
+    host        = self.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo -i",  
-      "yum install httpd -y",
-      "systemctl start httpd",
-      "systemctl enable httpd"
+
+      "sudo yum install httpd -y",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd"
     ]
   }
-    
-}  
+
+}
