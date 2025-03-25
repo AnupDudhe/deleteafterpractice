c0c999116972a8438560d92e037e6c2609a361d8
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-25 19:23:04 +0530

    sasa

--- terraformb47/datablock/amidata.tf
+++ terraformb47/datablock/amidata.tf
@@ -1,6 +1,13 @@
+provider "aws" {
+  region = "us-east-1" # Change to your desired region
+  profile = "configs"
+}
+
 data "aws_ami" "this_aws_ami" {
     name_regex       = "ami_use"
       filter {
     name   = "name"
     values = ["ami_use"]
     }
+
+}    
