aaa027d731c74996461e30634d290690ba41e101
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-25 19:55:20 +0530

    das

--- terraformb47/explicit/explicit.tf
+++ terraformb47/explicit/explicit.tf
@@ -44,3 +44,48 @@ resource "aws_security_group" "webserversg" {
   }
 
 }
+
+variable "this_instancetype" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "this_ami_id" {
+    type = string
+    default = "ami-05c179eced2eb9b5b"
+}
+
+variable "this_keyname" {
+    type = string
+    default = "b25"  
+
+}
+
+variable "this_tags" {
+    type = string
+    default = "webserver"
+}
+
+
+variable "this_count" {
+    type =  number 
+    default = 2
+}
+
+variable "this_availability_zone" {
+   type = string 
+   default = "ap-south-1b"
+}
+
+variable "this_b64" {
+   type = bool
+   default = true
+
+}
+
+
+ 
+
+
+
+
