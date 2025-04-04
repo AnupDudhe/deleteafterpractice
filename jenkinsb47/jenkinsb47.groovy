592107d6c05ed16a27a894fdf2a2694f15d90dbe
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-04-04 20:10:32 +0530

    sa

--- jenkinsb47/jenkinsb47.groovy
+++ jenkinsb47/jenkinsb47.groovy
@@ -6,12 +6,7 @@ pipeline {
                  git 'https://github.com/AnupDudhe/studentapp-ui.git'
             }
         }
-        stage('Test') {
-            steps {
-                 echo 'we are deploying in this stage'
-                //
-            }
-        }
+        
 
         stage('Build') {
             steps {
@@ -20,7 +15,19 @@ pipeline {
                 //
             }
         }
-             stage('Deploy') {
+        stage('Test') {
+            steps {
+                 echo 'we are deploying in this stage'
+                 sh '''
+                 mvn sonar:sonar   
+                 -Dsonar.projectKey=studentapp   
+                 -Dsonar.host.url=http://52.66.213.94:9000   
+                 -Dsonar.login=9719d6a02a9625636b841cc8bfb44b512246f562
+                 '''
+                //
+            }
+        }
+        stage('Deploy') {
             steps {
                 echo 'we are deploying in this stage' 
                 sh ''' 
