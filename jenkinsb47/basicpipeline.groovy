9bffcfe2ad92c255aa017ceae49a95ece52d58d8
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-04-04 19:42:52 +0530

    sa

--- jenkinsb47/basicpipeline.groovy
+++ jenkinsb47/basicpipeline.groovy
@@ -9,6 +9,12 @@ pipeline {
         stage('Test') {
             steps {
                  echo 'we are deploying in this stage'
+                sh ''' 
+                 mvn sonar:sonar \
+                 -Dsonar.projectKey=studentapp \
+                 -Dsonar.host.url=http://52.66.213.94:9000 \
+                 -Dsonar.login=9719d6a02a9625636b841cc8bfb44b512246f562
+                 '''
                 //
             }
         }
