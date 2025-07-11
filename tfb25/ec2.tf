0cc4682a2e97694299dabb2df8fd779050c36c4a
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-11 09:00:45 +0530

    te

--- tfb25/ec2.tf
+++ tfb25/ec2.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "web" {
-   ami="ami-05ee755be0cd7555c"
+   ami= "ami-05ee755be0cd7555c"
    instance_type = "t2.micro"
    vpc_security_group_ids = ["sg-0243699f25e16090b"] 
-   delete_on_termination=true
-   volume_type="gp2"
-   volume_size=10
+   delete_on_termination= true
+   volume_type= "gp2"
+   volume_size= 10
    
   tags = {
     Name = "webserver"
     purpose = "webserverinstalled"
   }
-  
+
 }
