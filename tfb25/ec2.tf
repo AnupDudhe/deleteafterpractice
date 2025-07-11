33ad234cb060edecee816d7512ed66a92c4e1d12
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-11 09:06:23 +0530

    rw

--- tfb25/ec2.tf
+++ tfb25/ec2.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "web" {
    ami = "ami-05ee755be0cd7555c"
    instance_type = "t2.micro"
    vpc_security_group_ids = ["sg-0243699f25e16090b"] 
-   delete_on_termination = false
    root_block_device {
     volume_size = 10
     volume_type = "gp3"
+    delete_on_termination = false
   }
    
   tags = {
