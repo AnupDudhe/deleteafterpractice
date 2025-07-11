21938e7f565914396abc408ae2db8f6c13a3adcc
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-11 09:16:34 +0530

    sa

--- tfb25/ec2.tf
+++ tfb25/ec2.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "this" {
    ami = "ami-05ee755be0cd7555c"
    instance_type = "t2.micro"
-   vpc_security_group_ids = ["sg-0243699f25e16090b"] 
+   vpc_security_group_ids = ["sg-0243699f25e16090b"]
    root_block_device {
     volume_size = 10
     volume_type = "gp3"
     delete_on_termination = false
-  }
-   
+}
+
   tags = {
     Name = "webserver"
     purpose = "webserverinstalled"
