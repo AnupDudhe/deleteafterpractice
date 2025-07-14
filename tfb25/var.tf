e998e2ad1c3ce2693f08ccc925ff4dab2a557d6a
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-14 09:07:12 +0530

    4

--- tfb25/var.tf
+++ tfb25/var.tf
@@ -21,27 +21,11 @@ variable "this_list" {
 
 variable "this_map" {
     type = map 
-    
     default = {
         voltype = "gp3" 
         secgroup = "sg-0243699f25e16090b"
         name = "webserver"
     }
-
-}
-
-variable "this_any" {
-    type = any 
-    default = {
-        count = 1
-        api_termination_ec2 = false
-        api_stop_ec2 = false 
-        ami_ec2 = "ami-05ee755be0cd7555c" 
-        instance_type_list = ["t2.micro" , "t3.small" , "t3.large"]
-        tags_map = {
-           purposeec2 = "webserver" 
-           name = "webserver"
-        }
 
 }
 
