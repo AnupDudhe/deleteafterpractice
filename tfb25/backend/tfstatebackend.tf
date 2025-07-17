8b182a92e4ce1ccd25ec48ff955c5714aecc13f8
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-17 08:47:36 +0530

    ew

---
+++ tfb25/backend/tfstatebackend.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+      region = "us-east-1"
+      profile = "configs"
+      shared_credentials_files = ["/home/anup/.aws/credentials"]
+    
+}
+
+#Partitionkey = LockID
+terraform {
+    backend "s3" {
+        bucket = "cbzbatchb25"
+        key = "terraform.tfstate"
+        dynamodb_table = "cbz25"
+        region = "us-east-1"
+        profile = "configs"
+        shared_credentials_files = ["/home/anup/.aws/credentials"]
+    }
+}
+
+
+resource "aws_instance" "ths_instance" {
+  ami = "ami-0150ccaf51ab55a51"
+  //key_name = ""
+  instance_type = "t2.micro"
+  //security_groups = [""]
+  count = 1
+  
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y apache2
+    sudo echo "hello world this is Batc24" >> /var/www/html/index.html
+    EOF
+   tags = {
+    Name = "first_terraform_instance"
+
+  } 
+    
+}
