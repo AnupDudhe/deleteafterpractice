520b54f610a7ff7b97855f57d4bb5bf950314ff3
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-21 08:31:42 +0530

    re

--- tfb25/loops/count.tf
+++ tfb25/loops/count.tf
@@ -1,1 +1,15 @@
 
+provider "aws" {
+    region =  "ap-south-1"
+    profile = "configs"
+}
+ 
+
+resource "aws_instance" "this_aws_instance" {
+    ami = "ami-0be5f59fbc80d980c"
+    //vpc_security_group_ids = ["sg-032e1a4a1685a03be"]
+    //key_name = "delete_oregon_anup"
+    instance_type = "t2.micro"
+    count = 3
+}   
+ 
