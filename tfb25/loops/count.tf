fcca9a35e389801932c5e385c8e98a9294b80471
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-21 08:40:53 +0530

    ew

--- tfb25/loops/count.tf
+++ tfb25/loops/count.tf
@@ -13,3 +13,15 @@ resource "aws_instance" "this_aws_instance" {
     count = 3
 }   
  
+variable "instance_ami" {
+  default = ["ami-05f991c49d264708f" , "ami-000c270573b170975"]
+}
+
+resource "aws_instance" "this_aws_instance_var" {
+    count = length(var.instance_ami) #2
+    ami = var.instance_ami[count.index] #01
+    //vpc_security_group_ids = ["sg-032e1a4a1685a03be"]
+    //key_name = "delete_oregon_anup"
+    instance_type = "t2.micro"
+   
+} 
