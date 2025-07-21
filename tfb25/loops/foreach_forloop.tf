7b3114fd80f8aba8a96fa128519d325e8e641f79
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-21 09:03:56 +0530

    sa

--- tfb25/loops/foreach_forloop.tf
+++ tfb25/loops/foreach_forloop.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "this_aws_instancenew" {
     tags = {
         name = "webserver"
     }
+    name = webserver
     
 }
 
@@ -14,4 +15,15 @@ variable "imageid" {
     type = list(string)
     default = ["ami-05f991c49d264708f" , "ami-0987e9d53da324257"]
 
+}
+
+resource "aws_iam_user" "main_user"{
+    for_each = toset(var.main_user_name)
+    name = each.value
+}
+
+variable "main_user_name" {
+    type = list(string)
+   
+    default = ["ubuntu","centos","redhat"]
 }
