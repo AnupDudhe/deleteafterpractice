6a28e03b92ada0eccc19a543ba6ea4eecaadfac0
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-21 09:00:27 +0530

    ds

---
+++ tfb25/loops/foreach_forloop.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "this_aws_instance" {
+    for_each = toset(var.imageid)
+    ami = each.value
+   // vpc_security_group_ids = ["sg-032e1a4a1685a03be"]
+   // key_name = "delete_oregon_anup"
+    instance_type = "t2.micro"
+    tags = {
+        name = webserver
+    }
+    
+}
+
+variable "imageid" {
+    type = list(string)
+    default = ["ami-05f991c49d264708f" , "ami-0987e9d53da324257"]
+
+}
