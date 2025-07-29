dba86b47d2d0cc2f7c9c2cb6f469f7a997aa16e1
Anup <avengersanup@gmail.com>
2025-07-29 08:57:34 +0530

    new

--- jenkinsb25/declarative.groovy
+++ jenkinsb25/declarative.groovy
@@ -1,1 +1,26 @@
-
+pipeline {
+    agent { label 'webserver-prod' }
+    stages {
+        stage('pull') {
+            steps {
+                echo "we are pulling the source code from git repository"
+                git 'https://github.com/AnupDudhe/studentapp-ui.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo "Building this app using maven"
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "Testing this app using Sonarqube"
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "Deploying this app to the prod server in apache tomcat"
+            }
+        }
+    }
+}
