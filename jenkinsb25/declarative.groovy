9326140f24bb19198b3dff8eab670720ec8e2b3c
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-07-31 09:05:02 +0530

    a

--- jenkinsb25/declarative.groovy
+++ jenkinsb25/declarative.groovy
@@ -4,12 +4,16 @@ pipeline {
         stage('pull') {
             steps {
                 echo "we are pulling the source code from git repository"
-                git 'https://github.com/AnupDudhe/studentapp-ui.git'
+                git branch: 'main', changelog: false, poll: false, url: 'https://github.com/AnupDudhe/cloudblitz-student-app.git'
+                //git 'https://github.com/AnupDudhe/studentapp-ui.git'
             }
         }
         stage('Build') {
             steps {
                 echo "Building this app using maven"
+                sh '''cd backend
+                mvn clean package -Dmaven.test.skip=true
+                aws s3 cp target/student-registration-backend-0.0.1-SNAPSHOT.jar s3://cbzstudentappversion/'''
             }
         }
         stage('Test') {
