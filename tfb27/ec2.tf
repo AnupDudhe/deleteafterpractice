e739f8601f8bff4d7b5006e2e400e311c186f94f
unknown <Anup@LAPTOP-4L0F33J3>
2025-08-26 21:28:24 +0530

    b27

--- tfb27/ec2.tf
+++ tfb27/ec2.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+     region = "eu-north-1"
+	 profile = "configs"
+}
 
+
+resource "aws_instance" "webserverresourceblock" {
+    ami = "ami-0c4fc5dcabc9df21d" 
+    instance_type =  "t2.micro" 
+    key_name = "b27stk"
+    security_groups =  ["sg-083d7eae0bf8a0d7d"]
+}
