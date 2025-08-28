a5f97520f59215925da19b7887db3425c3b6bcbb
unknown <Anup@LAPTOP-4L0F33J3>
2025-08-28 21:02:54 +0530

    new

--- tfb27/ec2.tf
+++ tfb27/ec2.tf
@@ -8,5 +8,36 @@ resource "aws_instance" "webserverresourceblock" {
     ami = "ami-0c4fc5dcabc9df21d" 
     instance_type =  "t2.micro" 
     key_name = "b27stk"
-    vpc_security_group_ids =  ["sg-083d7eae0bf8a0d7d"]
+    vpc_security_group_ids =  ["sg-083d7eae0bf8a0d7d" , aws_security_group.webserver_sg.id ]
+} 
+
+
+resource "aws_security_group" "webserver_sg" {
+       ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
