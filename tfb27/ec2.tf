40807b9862b59fac06e096c6d2b00397c54e4645
unknown <Anup@LAPTOP-4L0F33J3>
2025-08-28 21:15:54 +0530

    new

--- tfb27/ec2.tf
+++ tfb27/ec2.tf
@@ -8,7 +8,10 @@ resource "aws_instance" "webserverresourceblock" {
     ami = "ami-0c4fc5dcabc9df21d" 
     instance_type =  "t2.micro" 
     key_name = "b27stk"
-    vpc_security_group_ids =  ["sg-083d7eae0bf8a0d7d" , aws_security_group.webserver_sg.id ]
+    vpc_security_group_ids =  ["sg-083d7eae0bf8a0d7d" , aws_security_group.webserver_sg.id ] 
+    tags = {
+        Name = "webserverinstance"
+    }
 } 
 
 
