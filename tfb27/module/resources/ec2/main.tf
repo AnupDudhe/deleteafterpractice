b186887b13d44586ba53c34a3f9fa230dbe9f2f0
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-09-02 21:47:41 +0530

    d

--- tfb27/module/resources/ec2/main.tf
+++ tfb27/module/resources/ec2/main.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "this_inst" {
   disable_api_termination = var.This_api_termination
   key_name                = var.This_key
   #vpc_security_group_ids  = var.This_sg
+  subnet_id = var.this_aws_instance_subnet 
   tags = {
       Name = var.This_tag
      }
