6f16ed29461931c59d2d75d184804c81080ea64d
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-09-03 21:02:43 +0530

    Create backend.tf

---
+++ tfb27/tfstatebkp/backend.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+      region = "us-west-2"
+      profile = "configs"
+      shared_credentials_files = ["/home/anup/.aws/credentials"]
+    
+}
+
+#Partitionkey = LockID
+terraform {
+    backend "s3" {
+        bucket = "cbzbatchb25new"
+        key = "terraform.tfstate"
+        dynamodb_table = "cbz25"
+        region = "us-west-2"
+        profile = "configs"
+        shared_credentials_files = ["/home/anup/.aws/credentials"]
+    }
+}
+
+
+resource "aws_instance" "ths_instance" {
+  ami = var.this_aws_ami
+  //key_name = ""
+  instance_type = var.this_aws_instance_instance_type
+  count = var.this_aws_instance_count
+  
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y apache2
+    sudo echo "hello world this is Batc24" >> /var/www/html/index.html
+    EOF
+   tags = {
+    Name = "first_terraform_instance"
+
+  } 
+    
+}
