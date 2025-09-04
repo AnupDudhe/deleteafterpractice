de1b2ff92ba102a9f26a11bee268456feed89c9d
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-09-04 21:07:55 +0530

    sa

--- tfb27/loops/loops.tf
+++ tfb27/loops/loops.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+      region = "us-west-2"
+      profile = "configs"
+      shared_credentials_files = ["/home/anup/.aws/credentials"]
+    
+}
+
 resource "aws_instance" "this_aws_instancenew" {
     for_each = toset(var.imageid)
     ami = each.value
@@ -15,4 +22,12 @@ variable "imageid" {
     type = list(string)
     default = ["ami-01102c5e8ab69fb75" , "ami-03aa99ddf5498ceb9"]
 
+}
+
+
+output "aws_ec2" {
+  value = [
+    for amiid in var.imageid:
+        aws_instance.this_aws_instancenew[amiid].public_ip
+  ]
 }
