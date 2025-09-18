abd881b59a6ffb67d5b39ddbb76c156d49cdc11f
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-09-18 21:40:07 +0530

    Update basic.groovy

--- jenkinsb27/basic.groovy
+++ jenkinsb27/basic.groovy
@@ -25,6 +25,18 @@ pipeline {
             steps {
                 //
                 echo "we are deploying in this stage" 
+                	sh ''' 
+                    sudo curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.109/bin/apache-tomcat-9.0.109.zip
+                    sudo mv apache-tomcat-9.0.109.zip /opt/
+                    sudo apt-get install unzip -y
+                    sudo unzip /opt/apache-tomcat-9.0.109.zip -d /opt/
+                    sudo rm -rf /opt/apache-tomcat-9.0.109.zip
+                    sudo mv    /home/ubuntu/workspace/webserver/target/studentapp-2.2-SNAPSHOT.war /opt/apache-tomcat-9.0.109/webapps/studentapp.war
+                    sudo curl -O https://s3-us-west-2.amazonaws.com/studentapi-cit/mysql-connector.jar 
+                    sudo mv mysql-connector.jar /opt/apache-tomcat-9.0.109/lib/mysql-connector.jar
+                    sudo bash /opt/apache-tomcat-9.0.109/bin/catalina.sh stop  
+                    sudo bash /opt/apache-tomcat-9.0.109/bin/catalina.sh start 
+                    '''
             }
         }
     }
