e6dc19c6cc0cbe62c3b23b6f4094478051f6f5da
unknown <Anup@LAPTOP-4L0F33J3>
2025-10-07 09:42:05 +0530

    new

--- tfb28/ec2.tf
+++ tfb28/ec2.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "webserver" {
     ami =  var.webserver_ami
     instance_type = var.webserver_instance_type
-    vpc_security_group_ids = ["sg-0d1eccf2d4ec11307" , aws_security_group.sg-webserver.id ]
+    vpc_security_group_ids = ["sg-0d1eccf2d4ec11307" , aws_security_group.sg-webserver.id , data.aws_security_group.aws_sg.id ]
     disable_api_termination = var.webserver_api_termination
     count = var.webserver_copy  #loop count.
 user_data = <<-EOF
@@ -45,4 +45,11 @@ resource "aws_security_group"  "sg-webserver" {
     cidr_blocks = ["0.0.0.0/0"]
     
    }
+}
+
+
+
+data "aws_security_group" "aws_sg" {
+   name = "k8s-elb-a1c5032008df4484a803f431b42955ea"
+
 }
