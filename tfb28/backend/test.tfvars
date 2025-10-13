7f8ca4ccbab3abf3b8e34504251bf4051cc74731
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-10-13 10:11:59 +0530

    da

---
+++ tfb28/backend/test.tfvars
@@ -0,0 +1,5 @@
+
+this_aws_instance_instance_type = "t2.micro"
+this_aws_instance_count = 1
+this_aws_ami = "ami-052064a798f08f0d3"
+
---
+++ tfb28/backend/vars.tf
@@ -0,0 +1,8 @@
+variable "this_aws_instance_instance_type" {   
+}
+
+variable "this_aws_instance_count" {    
+}
+
+variable "this_aws_ami" {    
+}
