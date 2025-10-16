e75d704ab94919f342a4d11953d4a9bd28e374d6
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-10-16 10:25:14 +0530

    s

--- tfb28/eks/eks.tf
+++ tfb28/eks/eks.tf
@@ -39,7 +39,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [data.aws_subnet.default_subnet.id]
+    subnet_ids = [data.aws_subnet.default_subnet.ids]
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_policy_attachment]
