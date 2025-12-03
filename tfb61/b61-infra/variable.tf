aa38f1d55c783b4ddd2832da9e79552520977307
unknown <Anup@LAPTOP-4L0F33J3>
2025-12-03 14:48:51 +0530

    ew

--- tfb61/b61-infra/variable.tf
+++ tfb61/b61-infra/variable.tf
@@ -11,7 +11,7 @@ variable "webserver_vpc_security_group_ids" {
    default = "sg-0bbfe8e7d4bf3c179"
 }
 variable "webserver_disable_api_termination" {
-   default = true
+   default = false
 }
 variable "webserver_count" {
    default = 2
