75440e4e60b9c6d0dbc1ffdf834a9de870433510
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-12-10 14:21:25 +0530

    yes

--- tfb61/tfvars/var.tf
+++ tfb61/tfvars/var.tf
@@ -1,16 +1,16 @@
 variable "instance_type" {
-
+  default = "t3.medium"
 }
 
 variable "ami" {
-
+  default = "ami-068c0051b15cdb816"
 }
 
 variable "key_name" {
-
+  default = "30july"
 }
 
 
 variable "sg" {
-    
+  default = "sg-0cfbfd8e6fd3a7382"
 }
