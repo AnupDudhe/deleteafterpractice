820d31f0cd455c0f7b8771eacedd597ce6c55456
Dhananjay Khirdikar <dhananjaykhirdikar3@gmail.com>
2025-12-12 14:03:48 +0530

    sa

--- loopb61/count.tf
+++ loopb61/count.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "example" {
    count =  2
 }
 
+
+### creating servers with different os 
+resource "aws_instance" "example2" {
+   instance_type = "t3.micro"
+   ami = var.ami_list[count.index]
+   count =  length(var.ami_list)
+
+}
+
+variable "ami_list"{
+    default = ["ami-00ca570c1b6d79f36" , "ami-02b8269d5e85954ef"]
+                  #0                       #1
+}
