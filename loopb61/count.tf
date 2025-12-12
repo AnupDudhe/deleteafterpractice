4dba522ab5c64ab37aa70ed936215fef3db4235a
Dhananjay Khirdikar <dhananjaykhirdikar3@gmail.com>
2025-12-12 14:35:58 +0530

    wq

--- loopb61/count.tf
+++ loopb61/count.tf
@@ -22,4 +22,35 @@ resource "aws_instance" "example2" {
 variable "ami_list"{
     default = ["ami-00ca570c1b6d79f36" , "ami-02b8269d5e85954ef"]
                   #0                       #1
+} 
+
+
+#creating users 
+variable "userlist" {
+   default = ["user1" , "user2" , "user3"]
 }
+
+
+resource "aws_iam_user"  "userexample" {
+    name  = var.userlist[count.index]
+    count  = length(var.userlist)
+
+}
+
+
+
+/* output "instanceip" {
+    value = [
+        for amiid in var.ami_list :
+        aws_instance.example2[amiid].public_ip
+    ]
+  
+} */
+
+/* output "instanceip" {
+    value = [
+        for amiid in var.ami_list :
+        aws_instance.example2[amiid].id
+    ]
+  
+} */
