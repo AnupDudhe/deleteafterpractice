c26eb519ae6b0bc5d0cda079bcd5192336afb49f
Anup <avengersanup@gmail.com>
2026-01-30 09:40:54 +0530

    new

--- terraformb32/resource.tf
+++ terraformb32/resource.tf
@@ -34,7 +34,7 @@ resource "aws_security_group"  "webserversg" {
         cidr_blocks = ["0.0.0.0/0"]
       } 
 
-      egress = {
+      egress  {
         from_port = 0
         to_port = 0
         protocol = "-1"
