c7365297f175f9cece8eebb5eb238d9cc08ee3f3
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-03-23 13:28:11 +0530

    sa

---
+++ tfb64/complexdatatypes/ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+      region = "us-east-1"
+      profile = "configs"
+      shared_credentials_files = ["/home/anup/.aws/credentials"]
+    
+}
+
+resource "aws_instance" "example" {
+   instance_type = var.this_list.[0]
+    ami = var.this_map.amiws
+    key_name = var.this_any.wskeymaps.kp1
+   vpc_security_group_ids = [ "sg-0aecda28a625e1a5d" ]
+   count = var.this_any.wscountlist[0]
+}
---
+++ tfb64/complexdatatypes/var.tf
@@ -0,0 +1,31 @@
+variable "this_list" {
+    default = ["t3.micro"]
+                 #0         
+}
+
+variable "this_map" {
+    default = {
+      amiws = "ami-0c398cb65a93047f2" ,
+      keynamews = "default"
+
+    }
+}
+
+variable "this_any" {
+   default = {
+      wscount = 2 #var.this_any.wscount 
+      wsami = "ami-0c398cb65a93047f2" #var.this_any.wsami
+      wsinstancetype = ["t3.micro" , "t3.medium" , "t3.small"]
+      amiws = {
+         ami1ws = "ami-0c398cb65a93047f2" ,
+         ami2ws = "ami-0c398cb65a93047f3"
+      }
+      wscountlist = [1 , 2 , 3]
+      wskeymaps = {
+        kp1 = "ohiokey"
+        kp2 = "vpc"
+      }
+   }
+      
+
+}
