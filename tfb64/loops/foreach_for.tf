5a5269c12988abb8af88eeaf65b63456b7e5df72
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-03-23 13:56:36 +0530

    sa

--- tfb64/loops/foreach_for.tf
+++ tfb64/loops/foreach_for.tf
@@ -16,3 +16,23 @@ variable "this_instancetype" {
     default = ["t3.small" , "t3.micro" , "c7i-flex.large"]
 }
 
+
+
+resource "aws_instance" "example" {
+    count = 3
+    instance_type = "t3.micro"
+    ami = var.imageid
+    key_name = "ohiokey"
+   vpc_security_group_ids = ["sg-0aecda28a625e1a5d"]   
+}
+
+variable "imageid" {
+    default = "ami-0b0b78dcacbab728f"
+}
+
+output "aws_ec2" {
+  value = [
+    for amiid in var.imageid:
+        aws_instance.example[amiid].public_ip
+  ]
+}
