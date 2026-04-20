d7853a360c78d0d0813a5dd7e64bfc19510d1260
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-04-20 21:48:27 +0530

    sa

---
+++ jenkinsb34/jenkins.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent { label 'webserver' }
+    stages {
+        stage('Pull') {
+            steps {
+                git 'https://github.com/AnupDudhe/studentapp-ui.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'echo "this is build stage"'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'echo "this is test stage"'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'echo "this is deploy stage"'
+            }
+        }
+    }
+}
