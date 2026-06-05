748af6e8762cad5436322693d033afffb3217305
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-06-05 20:58:19 +0530

    sa

--- terraformb38/ec2.tf
+++ terraformb38/ec2.tf
@@ -5,11 +5,10 @@ provider "aws" {
 
 
 resource "aws_instance" "webserver" {
-   key_name =
-   ami = 
-   instance_type = 
-   vpc_security_group_ids =
-   disable_api_termination = 
+   key_name = "abhijeet"
+   ami = "ami-078f95be0757084a3" 
+   instance_type = "t3.micro" 
+   vpc_security_group_ids = ["sg-0e924451d7773195c"]
    tags = {
     Name = "Webserver"
   }
@@ -20,3 +19,4 @@ resource "aws_instance" "webserver" {
 #secret_key = "my-secret-key" 
 
 #aws configure --profile=configs 
+#disable_api_termination = true
