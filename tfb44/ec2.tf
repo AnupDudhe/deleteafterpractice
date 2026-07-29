560d81fde354cac0f791fb2573da273046801092
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-07-29 09:08:15 +0530

    sa

--- tfb44/ec2.tf
+++ tfb44/ec2.tf
@@ -9,10 +9,38 @@ provider "aws" {
 resource "aws_instance"  "webserver" {
     ami = "ami-004f790b835b26145" 
     instance_type = "t3.micro"
-    vpc_security_group_ids =  ["sg-0eae0d103615165a6"]
+    vpc_security_group_ids =  ["sg-0eae0d103615165a6" , "aws_security_group.webserver_sg.id" ]
     key_name = "key"
     tags = {
         purpose = "webserver"
     }
     count = 2 
+}
+
+
+resource "aws_security_group" "webserver_sg" {
+    name = "tf-sg"
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "HTTP" 
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 0
+      to_port = 65535
+      protocol = "-1" 
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+      from_port = 0
+      to_port = 65535
+      protocol = "-1" 
+      cidr_blocks  = ["0.0.0.0/0"]
+
+    }
+
 }
