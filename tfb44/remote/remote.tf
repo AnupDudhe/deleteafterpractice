c76a1c7c69710a299e36d28bd3b9a8c42f100a65
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-08-07 09:23:41 +0530

    sa

---
+++ tfb44/remote/remote.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "configs"
+} 
+
+#Partitionkey = LockID
+terraform {
+    backend "s3" {
+        bucket = "cbzb44"
+        key = "terraform.tfstate"
+        dynamodb_table = "cbzb44"
+        region = "us-east-1"
+        profile = "configs"
+        shared_credentials_files = ["/home/anup/.aws/credentials"]
+    }
+}
+
+
+
+resource "aws_instance"  "webserver" {
+    ami = var.amiid
+    instance_type = var.insttype
+    vpc_security_group_ids =  [ var.sg]
+    key_name = var.kp
+}
---
+++ tfb44/remote/var.tf
@@ -0,0 +1,16 @@
+variable "amiid" {
+    default = "ami-004f790b835b26145"
+}
+
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+   default = "sg-0eae0d103615165a6"
+}
+
+variable "kp" {
+    default = "key"
+
+}
