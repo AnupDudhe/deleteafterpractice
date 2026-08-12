e313ba0ab34146d064734f177059367835f50b59
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-08-12 09:00:14 +0530

    c

---
+++ tfb44/provisioners/ec2.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "configs"
+} 
+
+resource "aws_instance"  "webserver" {
+    ami = var.amiid
+    instance_type = var.insttype
+    vpc_security_group_ids =  [ var.sg]
+    key_name = var.kp
+
+
+provisioner "file" {
+    source      = "sample.txt"
+    destination = "/home/ec2-user/aws/"
+  }
+
+provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt" 
+}
+
+ connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("${path.module}/tf.pem")
+    host        = self.public_ip
+  }
+
+ provisioner "remote-exec" {
+    inline = [
+
+      "sudo yum install httpd -y",
+      "sudo systemctl start httpd",
+      "sudo apt update",
+      "sudo apt upgrade",
+      "sudo systemctl enable httpd"
+    ]
+  }
+
+
+
+
+
+}
