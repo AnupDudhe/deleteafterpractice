f6ee85731a24f5b5132a75acdc00bace112ea862
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-08-13 08:58:54 +0530

    gg

---
+++ tfb44/loop/resource.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "configs"
+} 
+
+
+resource "aws_instance"  "webserver" {
+    ami = var.amiid
+    instance_type = var.insttype
+    vpc_security_group_ids =  [ var.sg]
+    key_name = var.kp
+    count = 3  #identicalloops
+}
+
+
+
+resource "aws_instance"  "webserver" {
+    for_each = toset(var.imageid)    #unidenticalloops
+    ami = each.value
+    instance_type = var.insttype
+    vpc_security_group_ids =  [ var.sg]
+    key_name = var.kp
+
+}
+
+
+variable "imageid" {
+   default = ["ami-035827357e3c7e810" , "ami-01a00762f46d584a1" , "ami-0340d2c0eb945bf80"]
+}
+
+
---
+++ tfb44/loop/var.tf
@@ -0,0 +1,16 @@
+variable "amiid" {
+    default = "ami-0bdc7d025135d7b49"
+}
+
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+   default = "sg-0eae0d103615165a6"
+}
+
+variable "kp" {
+    default = "kp"
+
+}
